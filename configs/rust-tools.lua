3c85d31eb0bf4f178b325409e91af6f7d4853af4
Pavel Stejskal <pavel.stejskal@adastragrp.com>
2023-12-08 14:48:07 +0100

    dap for macos only

--- configs/rust-tools.lua
+++ configs/rust-tools.lua
@@ -4,17 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 -- Update this path
 local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/"
 local codelldb_path = extension_path .. "adapter/codelldb"
-local liblldb_path = extension_path .. "lldb/lib/liblldb"
-local this_os = vim.loop.os_uname().sysname
-
--- The path in windows is different
-if this_os:find "Windows" then
-  codelldb_path = extension_path .. "adapter\\codelldb.exe"
-  liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-else
-  -- The liblldb extension is .so for linux and .dylib for macOS
-  liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
-end
+local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
 
 -- Rust tools -> sets LSP as well
 local opts = {
